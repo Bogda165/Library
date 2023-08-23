e8244341f358e10d479f1e3515f89455e532339a
Bogda165 <koval.b@icloud.com>
2023-08-23 19:03:57 +0200

    Magazine.cpp

--- Library/Magazine.cpp
+++ Library/Magazine.cpp
@@ -6,3 +6,35 @@
 //
 
 #include "Magazine.hpp"
+
+Magazine::Magazine(){
+    
+}
+Magazine::Magazine(string _name, string _author, int _date):
+Publication(_name, _author, _date){
+    
+}
+
+
+void Magazine::add(string article){
+    articles.push_back(article);
+}
+void Magazine::del(int index){
+    articles.erase(articles.begin() + index);
+}
+std::vector<string> Magazine::get()const{
+    return articles;
+}
+
+void Magazine::show()const{
+    cout << "----------Magazine----------\n";
+    cout << "   Name : " << name << endl;
+    cout << "   Author's name : " << author << endl;
+    cout << "   Creation year : " << date << endl;
+    cout << "   List of articles : " << endl << endl;
+    for(int i = 0; i < articles.size(); i++){
+        cout << "       Article number " << i + 1 << " :\n";
+        cout << "         " << articles[i] << endl;
+    }
+    cout << endl;
+}
