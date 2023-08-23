03e0d92affa446d8b6bf6effe9f11afd786e5631
Bogda165 <koval.b@icloud.com>
2023-08-23 18:46:52 +0200

    book.cpp

--- Library/Book.cpp
+++ Library/Book.cpp
@@ -6,3 +6,27 @@
 //
 
 #include "Book.hpp"
+
+Book::Book(){
+    summary = "None";
+}
+
+Book::Book(string _name, string _author, int _date, string _summary):
+    Publication(_name, _author, _date){
+        summary = _summary;
+}
+
+void Book::setSummary(string _summary){
+    summary = _summary;
+}
+string Book::getSummary()const{
+    return summary;
+}
+
+void Book::show()const{
+    cout << "------------Book------------\n";
+    cout << "   Name : " << name << endl;
+    cout << "   Author's name : " << author << endl;
+    cout << "   Creation year : " << date << endl;
+    cout << endl;
+}
