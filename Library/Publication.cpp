f4f15b0bfa5354b0a2fb1438499bc308c877a5e6
Bogda165 <koval.b@icloud.com>
2023-08-23 18:35:39 +0200

    Publication.cpp

--- Library/Publication.cpp
+++ Library/Publication.cpp
@@ -1,8 +1,32 @@
-//
-//  Publication.cpp
-//  Library
-//
-//  Created by Богдан Коваль on 23.08.2023.
-//
-
 #include "Publication.hpp"
+
+Publication::Publication(){
+    name = "None";
+    author = "None";
+    date = 2020;
+}
+Publication::Publication(string _name, string _author, int _date){
+    name = _name;
+    author = _author;
+    date = _date;
+}
+
+void Publication::setName(string _name){
+    name = _name;
+}
+void Publication::setAuthor(string _author){
+    author = _author;
+}
+void Publication::setDate(int _date){
+    date = _date;
+}
+
+string Publication::getName()const{
+    return name;
+}
+string Publication::getAuthro()const{
+    return author;
+}
+int Publication::getDate()const{
+    return date;
+}
