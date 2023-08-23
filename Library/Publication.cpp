c1cd126b5838eaac607eaa834983d22fd9c273b3
Bogda165 <koval.b@icloud.com>
2023-08-23 18:31:44 +0200

    Publication.hpp

---
+++ Library/Publication.cpp
@@ -0,0 +1,8 @@
+//
+//  Publication.cpp
+//  Library
+//
+//  Created by Богдан Коваль on 23.08.2023.
+//
+
+#include "Publication.hpp"
---
+++ Library/Publication.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+class Publication{
+protected:
+    string name;
+    string author;
+    int date;
+    
+public:
+    Publication();
+    Publication(string _name, string _author, int _date);
+    
+    void setName(string _name);
+    void setAuthor(string _author);
+    void setDate(int _date);
+    
+    string getName()const;
+    string getAuthro()const;
+    int getDate()const;
+    
+    
+    virtual void show()const = 0;
+    
+};
